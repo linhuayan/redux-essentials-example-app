fcb3667497163f83c8ff3e409818caab5f3e9439
v_huaylin <v_huaylin@tencent.com>
2023-01-13 16:54:19 +0800

    feat: 数据流基础

---
+++ src/features/posts/SinglePostPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const SinglePostPage = () => {
+    return (
+        <section>
+            <article className='post'>
+                <h2></h2>
+            </article>
+        </section>
+    )
+}
