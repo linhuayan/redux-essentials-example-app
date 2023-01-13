362ba988bd58d6dd74ccabb04ead98523b7eac35
v_huaylin <v_huaylin@tencent.com>
2023-01-13 17:51:34 +0800

    feat: 添加单个文章的路由

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
-export const SinglePostPage = () => {
+export const SinglePostPage = ({ match }) => {
+    console.log('props');
+    const { postId } = match.params
+
+    const post = useSelector(state =>
+        state.posts.find(post => post.id === postId)
+    )
+
+    console.log('post', post)
+
+    if (!post) {
+        return (
+            <section>
+                <h2>页面未找到！</h2>
+            </section>
+        )
+    }
+
     return (
         <section>
             <article className='post'>
-                <h2></h2>
+                <h2>{post.title}</h2>
+                <p className='post-content'>{post.content}</p>
             </article>
         </section>
     )
